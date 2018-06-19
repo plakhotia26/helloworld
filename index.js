e1cdd9144a8791e2219e725cc9dd789dac9f240e
pranav.lakhotia@accenture.com <pranav.lakhotia@accenture.com>
2018-06-19 12:49:44 +0100

    500 error code

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var app = express();
 app.get('/', function (req, res) {
   if (!req.query.id) {
   res.send('Hello World!!\n');
-} } );
+} else 
+{ res.end(500); }
+} );
 
 app.listen(PORT)
 console.log('Running on http://localhost:' + PORT);
