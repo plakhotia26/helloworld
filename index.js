59fd8607b86cc21db8aba0ad2941f29244d3d62f
pranav.lakhotia@accenture.com <pranav.lakhotia@accenture.com>
2018-06-19 12:52:05 +0100

    next (error)

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 
 // App
 var app = express();
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
   if (!req.query.id) {
   res.send('Hello World!!\n');
 } else 
-{ res.end(500); }
+{ next(error); }
 } );
 
 app.listen(PORT)
